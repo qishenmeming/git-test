50da78181cfe65deb00796b53a39bc87b63525df
cyq <cyq@qq.com>
2022-04-25 14:10:01 +0800

    push commit

--- src/main/java/com/bean/student.java
+++ src/main/java/com/bean/student.java
@@ -9,5 +9,8 @@ public class student {
 
     public static void main(String[] args) {
         System.out.println("111111111111");
+
+        System.out.println("222222222222");
+        System.out.println("push commit");
     }
 }
