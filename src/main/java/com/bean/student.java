1c8c42511d739dd1fb91e4c94c1de0d184cb188d
cyq <cyq@qq.com>
2022-04-25 13:34:00 +0800

    11111

---
+++ src/main/java/com/bean/student.java
@@ -0,0 +1,13 @@
+package com.bean;
+
+/**
+ * @author chen
+ * @create 2022-04-25 13:29
+ */
+public class student {
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+    }
+}
